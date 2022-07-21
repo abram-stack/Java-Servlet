5fb0e1214ef1826f05bc10ac4fddbdb0c1ce096e
Anthony Bram Bimo <58656232+abram-stack@users.noreply.github.com>
2022-07-21 15:16:11 +0200

    Update Dish.java

--- src/Dish.java
+++ src/Dish.java
@@ -1,1 +1,31 @@
 //Dish java test
+public class Dish(){
+  private String dishName;
+  private int nutrition;
+  private double price;
+  
+  
+  //constructor
+  public Dish(String dishName, int nutrition, double price){
+    this.dishName = dishName;
+    this.nutrition = nutrition;
+    this.price = price;
+  }
+  
+  //getter and setter 
+  public String getDishName(){
+    return dishName;
+  }
+  
+  public int getNutrition(){
+    return nutrition;
+  }
+  
+  public double getPrice(){
+    return price;
+  }
+  
+  public void changePrice(double newPrice){
+    this.price = newPrice;
+  }
+}
